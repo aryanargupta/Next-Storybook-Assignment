7a40a65836af4d73d2e947bde38155d7b78d1f8a
Aryan Amit Gupta <aryanargupta@gmail.com>
2024-02-11 14:06:19 +0530

    Commit #7: Chart component commpleted

---
+++ components/Chart.stories.js
@@ -0,0 +1,17 @@
+import Charts from "./Chart";
+
+export default {
+  component: Charts,
+  title: "components/Charts",
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    id: "1",
+    super_healthy_camera: 56,
+    healthy_camera: 35,
+    warning_camera: 27,
+    danger_camera: 6,
+  },
+};
